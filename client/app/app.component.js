afbbbad5b04316e6032f71925f659151800f6910
Bruno Lopes <bruno_lopesc@yahoo.com.br>
2017-02-05 16:06:44 -0200

    requisicao http

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -5,9 +5,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 var AppComponent = (function () {
-    function AppComponent() {
+    //Construtor
+    function AppComponent(http) {
+        var _this = this;
+        //Propriedade fotos - array de objetos que guardará as fotos
+        this.fotos = [];
+        //Obtem resposta do servidor
+        http.get("http://localhost:3000/v1/fotos")
+            .map(function (res) { return res.json(); }) //Mapeia retornando a resposta em JSON
+            .subscribe(function (fotos) {
+            //Coloca o retorno na propriedade
+            _this.fotos = fotos;
+        }, function (erro) { return console.info(erro); } //Imprime o erro
+        );
     }
     return AppComponent;
 }());
@@ -16,7 +32,8 @@ AppComponent = __decorate([
         moduleId: module.id,
         selector: "app",
         templateUrl: "./app.component.html"
-    })
+    }),
+    __metadata("design:paramtypes", [http_1.Http])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
